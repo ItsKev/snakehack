b9b80fffe6b61a108f6671b3a45afbe8b7656c4b
Kevin Huber <kevin.huber@hotmail.com>
2017-11-11 13:04:33 +0100

    Fixed json parsing

--- src/main/java/com/github/stairch/rest/SnakeService.java
+++ src/main/java/com/github/stairch/rest/SnakeService.java
@@ -4,12 +4,13 @@ import com.github.stairch.dtos.*;
 import com.github.stairch.types.HeadType;
 import com.github.stairch.types.Move;
 import com.github.stairch.types.TailType;
-import com.google.gson.Gson;
+import com.google.gson.*;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.github.stairch.RestInPeace.BASE_URI;
 
@@ -37,10 +38,10 @@ public class SnakeService {
         final StartResponseDTO startResponse = new StartResponseDTO();
         startResponse.setColor("black");
         startResponse.setHeadUrl(BASE_URI + "static/head.png");
-        startResponse.setName("STAIR Java Snake");
+        startResponse.setName("Kevs Snake");
         startResponse.setTaunt("Meep meep");
 
-        startResponse.setHeadType(HeadType.getPixel());
+        startResponse.setHeadType(HeadType.getFang());
         startResponse.setTailType(TailType.getBlockbum());
         final String responseBody = gson.toJson(startResponse);
         return Response.status(Response.Status.OK).entity(responseBody).build();
@@ -50,14 +51,63 @@ public class SnakeService {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/move")
-    public final Response move(final StartRequestDTO startRequestDTO) {
-        System.out.println(startRequestDTO);
+    public final Response move(String string) {
+        MoveRequestDTO moveRequestDTO = this.getMoveRequestDTO(string);
 
         final MoveResponseDTO moveResponse = new MoveResponseDTO();
-        moveResponse.setMove(Move.left);
+        moveResponse.setMove(Move.right);
 
 
         final String responseBody = gson.toJson(moveResponse);
         return Response.status(Response.Status.OK).entity(responseBody).build();
+    }
+
+    private MoveRequestDTO getMoveRequestDTO(String string) {
+        MoveRequestDTO moveRequestDTO = gson.fromJson(string, MoveRequestDTO.class);
+        JsonParser jsonParser = new JsonParser();
+        JsonObject json = (JsonObject) jsonParser.parse(string);
+
+        JsonArray snakes = json.get("snakes").getAsJsonArray();
+        for (SnakeDTO snakeDTO : moveRequestDTO.getSnakes()) {
+            this.saveCoordsForSnake(snakes, snakeDTO);
+        }
+
+        JsonArray food = json.get("food").getAsJsonArray();
+        ArrayList<PointDTO> pointDTOS = new ArrayList<>();
+        for (JsonElement jsonElement : food) {
+            System.out.println(jsonElement);
+            JsonArray foodCoords = jsonElement.getAsJsonArray();
+            PointDTO pointDTO = new PointDTO();
+            pointDTO.setX(foodCoords.get(0).getAsInt());
+            pointDTO.setY(foodCoords.get(1).getAsInt());
+            pointDTOS.add(pointDTO);
+        }
+        moveRequestDTO.setFood(pointDTOS);
+
+        JsonArray deadSnakes = json.get("dead_snakes").getAsJsonArray();
+        for (SnakeDTO snakeDTO : moveRequestDTO.getDeadSnakes()) {
+            this.saveCoordsForSnake(deadSnakes, snakeDTO);
+        }
+
+        return moveRequestDTO;
+    }
+
+    private void saveCoordsForSnake(JsonArray snakes, SnakeDTO snakeDTO) {
+        for (JsonElement snake : snakes) {
+            JsonElement id = snake.getAsJsonObject().get("id");
+            if (snakeDTO.getId().equals(id.getAsString())) {
+                JsonObject asJsonObject = snake.getAsJsonObject();
+                JsonArray coords = asJsonObject.get("coords").getAsJsonArray();
+                List<PointDTO> pointDTOS = new ArrayList<>();
+                for (JsonElement coord : coords) {
+                    JsonArray coordArray = coord.getAsJsonArray();
+                    PointDTO pointDTO = new PointDTO();
+                    pointDTO.setX(coordArray.get(0).getAsInt());
+                    pointDTO.setY(coordArray.get(1).getAsInt());
+                    pointDTOS.add(pointDTO);
+                }
+                snakeDTO.setCoordinates(pointDTOS);
+            }
+        }
     }
 }
